6a1979e675e5a0fb4de73c7c7a02926a67cfd739
КахаровЖ <@N215-2>
2020-01-28 21:24:26 +0600

    Add project files.

---
+++ Models/Repository/EFDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Data.Entity;
+
+namespace GameStore.Models.Repository
+{
+    public class EFDbContext : DbContext
+    {
+        public EFDbContext()
+            : base(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\КахаровЖ\Desktop\GameStore\App_Data\MyDB.mdf;Integrated Security=True;Connect Timeout=30")
+        {  }
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
